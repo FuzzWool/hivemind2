d565604fb14ee4270cc4d1ac7119cd3bfeb30dc3
FuzzWool <sam.tom.drake@gmail.com>
2013-11-02 18:42:15 +0000

    ~ moved WorldMap to .game

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from code.sfml_plus import Window
 from code.sfml_plus import key
 
-window = Window((600,300), "Hivemind - Demo 2")
+ROOM_WIDTH, ROOM_HEIGHT = w = 600,300
+window = Window((1200, 600), "Hivemind - Demo 2")
+
+from code.game import WorldMap
+WorldMap = WorldMap(2,2)
 
 while window.is_open:
 	if window.is_focused:
-		if key.ENTER.pressed(): print 1
+		if key.ENTER.pressed():
+			pass
 
 	window.clear((255,220,0))
+	WorldMap.draw(window)
 	window.display()
