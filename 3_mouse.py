a9530ac1af08d489d9f27767b9ab29c77d120935
FuzzWool <sam.tom.drake@gmail.com>
2013-11-15 17:02:50 +0000

    + sfml_plus.Mouse: GameRectangle + held, pressed, released
    ! sfml_plus Mouse
    + Mouse inherits from GameRectangle
    + Mouse has held, pressed, released
    + window reset
    
    ! clean
    ! combine with key
    ! handle inside window

--- 3_mouse.py
+++ 3_mouse.py
@@ -5,10 +5,12 @@ from code.sfml_plus import key
 
 from sfml import Mouse as _Mouse
 from code.sfml_plus import Rectangle
+
 class Mouse(Rectangle):
 
 	def __init__(self, window):
 		self.window = window
+		self._init_buttons()
 
 	@property
 	def x(self):
@@ -19,14 +21,65 @@ class Mouse(Rectangle):
 
 	#
 
-	def left_held(self):
+	# def left_held(self):
+	# 	return _Mouse.is_button_pressed(_Mouse.LEFT)
+
+	# def right_held(self):
+	# 	return _Mouse.is_button_pressed(_Mouse.RIGHT)
+
+	# def middle_held(self):
+	# 	return _Mouse.is_button_pressed(_Mouse.MIDDLE)
+
+	##### BUTTON EVENTS
+
+	def _init_buttons(self): #init
+		self.left = left()
+		self.right = right()
+		self.middle = middle()
+
+	def reset_buttons(self):
+		self.left.reset()
+		self.right.reset()
+		self.middle.reset()
+
+class _Button: #virtual
+# . Monitors a 'held' state. Override it.
+# * Notes if it has just been pressed or released.
+# (Needs to be looped so it refreshes cleanly.)
+	
+	def held(self): return False
+
+	#
+
+	was_pressed = False
+	def pressed(self):
+		if self.held() and not self.was_pressed:
+			return True
+		return False
+
+	def released(self):
+		if not self.held() and self.was_pressed:
+			return True
+		return False
+
+	#
+
+	def reset(self): #Must be called at the end of a loop.
+		self.was_pressed = self.held()
+
+class left(_Button):
+	def held(self):
 		return _Mouse.is_button_pressed(_Mouse.LEFT)
 
-	def right_held(self):
+class right(_Button):
+	def held(self):
 		return _Mouse.is_button_pressed(_Mouse.RIGHT)
 
-	def middle_held(self):
+class middle(_Button):
+	def held(self):
 		return _Mouse.is_button_pressed(_Mouse.MIDDLE)
+
+
 
 ############
 
@@ -35,8 +88,10 @@ mouse = Mouse(window)
 
 while window.is_open:
 	if window.is_focused:
-		if mouse.left_held():
+		if mouse.left.released():
 			print mouse.position
+
+	mouse.reset_buttons()
 
 	window.clear((255,220,0))
 	window.display()
