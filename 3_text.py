f7068cd7894fc8cebb9908b23ad2f492b9c0c160
FuzzWool <message.me.sam@gmail.com>
2013-11-21 11:12:17 +0000

    + each letter is an individual class (stores position/letter)
    ! advanced Bitmap text class
    + each letter is an individual class (stores position/letter)
    
    ! kerning
    ! may be moved around
    
    ! color
    
    ! animation
    ! shake individual words
    
    ! make individual letters pop up
    ! fading intervals
    ! movement

--- 3_text.py
+++ 3_text.py
@@ -7,6 +7,7 @@ from sfml import Drawable
 from sfml import Vertex, VertexArray
 from sfml import PrimitiveType, RenderStates
 from sfml import Texture
+from code.sfml_plus import Rectangle
 
 class Text(Drawable):
 #A text class which follows the sfml_plus standard.
@@ -22,49 +23,41 @@ class Text(Drawable):
 	string = ""
 	def write(self, string):
 		self.string = string
+		self._create_letters()
 		self._create_vertex_array()
 
 	#
+
+	def _create_letters(self): #write
+		letters = []
+
+		x,y = 0,0
+		w,h = 8,12
+		for character in self.string:
+
+
+			if character == "\n":
+				x = 0
+				y += 1
+			else:
+				points = (x*w, y*h, (x+1)*w, (y+1)*h)
+				Letter = self.Letter(points, character)
+				letters.append(Letter)
+				x += 1
+
+		self.letters = letters
 
 	def _create_vertex_array(self): #write
 		s = PrimitiveType.QUADS
 		vertex_array = VertexArray(s)
 		
-		string = self.string
-		characters = "abcdefghijklmnopqrstuvwxyz "
-		grammar = ".:,;-!"
+		#
 
-		### POINT
-		w, h = 8,12
-		x, y = 0,0
-		for c in string:
+		for Letter in self.letters:
+			Letter.create_vertex()
+			for vertice in Letter.vertex:
+				vertex_array.append(vertice)
 
-			if c == "\n":
-				x = 0
-				y += 1
-
-			else:
-
-				points = self.points()
-
-				w, h = 8,12
-				x1, y1, x2, y2 = w*x,h*y,w*(x+1),h*(y+1)
-				points.points(x1,y1,x2,y2)
-
-				if c in characters:
-					xc = characters.index(c)
-					points.clip(xc,1)
-				elif c in characters.upper():
-					xc = characters.upper().index(c)
-					points.clip(xc,0)
-				elif c in grammar:
-					xc = grammar.index(c)
-					points.clip(xc,2)
-
-				for point in points():
-					vertex_array.append(point)
-			
-				x += 1
 		#
 
 		self.vertex_array = vertex_array
@@ -73,29 +66,51 @@ class Text(Drawable):
 		self.render_states.texture = t
 
 
-	class points:
-	#WIP - Easy positioning/clipping for vertices.
-		
-		def __init__(self):
-			self._p = []
-			for i in range(4): self._p.append(Vertex())
 
-		def points(self, x1, y1, x2, y2):
-			self._p[0].position = x1,y1
-			self._p[1].position = x2,y1
-			self._p[2].position = x2,y2
-			self._p[3].position = x1,y2
-			self.w, self.h = x2-x1, y2-y1
+	class Letter(Rectangle):
+	# * positioning.
+	# * clipping.
 
-		def clip(self, x, y):
-			x1, y1 = x*self.w, y*self.h
+		def __init__(self, points, letter):
+			self.points = points
+			self.letter = letter
+
+
+		characters = "abcdefghijklmnopqrstuvwxyz "
+		grammar = ".:,;-!"
+
+		def create_vertex(self):
+			vertex = []
+			for i in range(4): vertex.append(Vertex())
+
+			#position
+			vertex[0].position = self.x1, self.y1
+			vertex[1].position = self.x2, self.y1
+			vertex[2].position = self.x2, self.y2
+			vertex[3].position = self.x1, self.y2
+
+			#clipping
+			x, y = 26,0
+			l = self.letter
+
+			if l in self.characters:
+				x = self.characters.index(l)
+				y = 1
+			elif l in self.characters.upper():
+				x = self.characters.upper().index(l)
+				y = 0
+			elif l in self.grammar:
+				x = self.grammar.index(l)
+				y = 2
+
+			x1, y1 = self.w*x, self.h*y
 			x2, y2 = x1+self.w, y1+self.h
-			self._p[0].tex_coords = x1,y1
-			self._p[1].tex_coords = x2,y1
-			self._p[2].tex_coords = x2,y2
-			self._p[3].tex_coords = x1,y2
+			vertex[0].tex_coords = x1, y1
+			vertex[1].tex_coords = x2, y1
+			vertex[2].tex_coords = x2, y2
+			vertex[3].tex_coords = x1, y2
 
-		def __call__(self): return self._p
+			self.vertex = vertex
 
 
 #################################
@@ -103,7 +118,7 @@ class Text(Drawable):
 Window = Window((1200,600), "Text Class")
 
 Text = Text()
-Text.write("Hello there, my name is Sam.\nAha - Ahahaha!")
+Text.write("Hello there, my name is Sam.\nThis is a test!")
 
 while Window.is_open:
 	if Window.is_focused:
