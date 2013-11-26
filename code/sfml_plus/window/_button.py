cd713d5423d3145f316359d46f18ba17e05d909d
FuzzWool <sam.tom.drake@gmail.com>
2013-11-26 21:32:07 +0000

    ~ fixed button glitch

--- code/sfml_plus/window/_button.py
+++ code/sfml_plus/window/_button.py
@@ -8,17 +8,16 @@ class _Button: #virtual
 	#
 
 	was_held = False
-	def pressed(self):
-		if self.held() and not self.was_held:
-			return True
-		return False
 
-	def released(self):
-		if not self.held() and self.was_held:
-			return True
-		return False
+	_pressed = False
+	def pressed(self): return self._pressed
+
+	_released = False
+	def released(self): return self._released
 
 	#
 
 	def reset(self): #Must be called at the end of a loop.
+		self._pressed =(self.held() and not self.was_held)
+		self._released =(not self.held() and self.was_held)
 		self.was_held = self.held()
