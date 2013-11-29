7b4f832841e5f972080aebedc90e41863a7309d2
FuzzWool <sam.tom.drake@gmail.com>
2013-11-29 14:37:55 +0000

    . designed Level Editor Toolbox

--- 1_uibox.py
+++ 1_uibox.py
@@ -1,6 +1,46 @@
 from code.sfml_plus import Window
 from code.sfml_plus import Key
 
+#######################################
+
+class _UIBox:
+	pass
+
+class _UIDropdown:
+	pass
+
+###############
+
+class UIBox:
+
+	def controls(self):
+		pass
+
+	def draw(self):
+		pass
+
+	#
+
+	def open(self):
+		pass
+
+	def close(self):
+		pass
+
+	#####
+
+class UIDropdown:
+
+	def controls(self):
+		pass
+
+	def draw(self):
+		pass
+
+
+#######################################
+
+
 Window = Window((1200,600), "UI Box (Tile)")
 
 while Window.is_open:
