b3566309e36fb9c8c7913383dcaabae76eddfa51
FuzzWool <sam.tom.drake@gmail.com>
2013-12-15 19:45:41 +0000

    . new UI planning

--- 2_new_uibox.py
+++ 2_new_uibox.py
@@ -1,11 +1,44 @@
 from code.sfml_plus import Window
 from code.sfml_plus import Key
+from code.sfml_plus import Mouse
+##########################################
 
+from code.sfml_plus import Rectangle
+
+class _UI(Rectangle):
+# Communication
+
+	x,y,w,h = 0,0,0,0
+	graphics = []
+
+	def controls(self, Key, Mouse, Camera):
+		pass
+
+	def draw(self, Window):
+		pass
+
+
+class Box(_UI):
+# Graphics
+	pass
+
+class Button(_UI):
+# State Handling
+	pass
+
+
+##########################################
 Window = Window((1200,600), "Untitled")
+Mouse = Mouse(Window)
+
+box1 = Box()
 
 while Window.is_open:
 	if Window.is_focused:
 		if Key.ENTER.pressed(): print 1
 
+		box1.controls(Key, Mouse, None)
+
 	Window.clear((255,220,0))
-	Window.display()
+	box1.draw(Window)
+	Window.display(Mouse)
