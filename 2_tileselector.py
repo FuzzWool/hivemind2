e48c69830ccb2ab36726f2d4c691309a0ec14bb0
FuzzWool <sam.tom.drake@gmail.com>
2013-12-30 16:04:55 +0000

    + Sheet

--- 2_tileselector.py
+++ 2_tileselector.py
@@ -5,10 +5,62 @@ from code.sfml_plus import Mouse
 ##########################################
 
 from code.sfml_plus.ui import Box
+from code.sfml_plus import Texture, MySprite
+from code.sfml_plus import Rectangle
 
 class TileSelector(Box):
-	pass
+	
+	#################################
+	# PUBLIC
 
+	def __init__(self):
+		Box.__init__(self)
+		self.Sheet = self.Sheet("0.png")
+		self.size = self.Sheet.size
+
+	def draw(self, target, states):
+		self._parent_Sheet()
+		Box.draw(self, target, states)
+		self.Sheet.draw(target, states)
+
+	#################################
+	# PRIVATE
+
+	# SHEET
+	def _parent_Sheet(self):
+		#alpha
+		self.Sheet.alpha = self.alpha
+		#move
+		x_move = self.x - self.old_pos[0]
+		y_move = self.y - self.old_pos[1]
+		self.Sheet.x += x_move
+		self.Sheet.y += y_move
+
+
+	class Sheet(Rectangle):
+
+		def __init__(self, name):
+			Rectangle.__init__(self)
+			self._create_sprite(name)
+
+		def draw(self, target, states):
+			self._parent_sprite()
+			self._draw_sprite(target, states)
+		#
+
+		_sprite = None
+		def _create_sprite(self, name):
+			t = Texture.from_file("assets/tilesheets/"+name)
+			self._sprite = MySprite(t)
+			self.size = self._sprite.size
+		#
+		def _parent_sprite(self):
+			c = self._sprite.color
+			c.a = self.alpha
+			self._sprite.color = c
+			self._sprite.position = self.position
+		def _draw_sprite(self, target, states):
+			target.draw(self._sprite)
 
 
 
@@ -22,7 +74,7 @@ Window = Window((1200,600), "Untitled")
 Mouse = Mouse(Window)
 
 box1 = Box()
-box1.w += 100; box1.h += 100
+box1.w += 250; box1.h += 100
 box1.center = Window.center
 
 box2 = Accept_Button()
@@ -31,6 +83,7 @@ box2.y += (box1.h - box2.h) - box2.rise
 box1.children.append(box2)
 
 tileselector = TileSelector()
+tileselector.x += 25; tileselector.y += 20
 box1.children.append(tileselector)
 
 ##########################################
