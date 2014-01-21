23ceae3009aab00d2a9a48078e6a85d91073918d
FuzzWool <sam.tom.drake@gmail.com>
2014-01-21 21:31:00 +0000

    ~ moved

--- code/sfml_plus/ui/__init__.py
+++ code/sfml_plus/ui/__init__.py
@@ -1,6 +1,6 @@
 from _ui import _UI
 #
-from boxes import Box, MaskBox
+from boxes import Box, MaskBox, InputBox
 from buttons import Button, ToggleButton, Cancel_Button, Accept_Button
 from sliders import Horizontal_Slider, Vertical_Slider, SliderBox
 from dropdown import Dropdown
